7767836b9c8ac605068f3336490f809f280c391c
JEEVAN <jeevanmogalipuvvu@gmail.com>
2024-11-12 07:02:49 +0530

    Initial Commit

---
+++ backend/mycourses.js
@@ -0,0 +1,21 @@
+// getting-started.js
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect(prcess.env.MONGODB_URI);
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+
+const mycourseSchema = new mongoose.Schema({
+    name: String,
+    img: String,
+    price: String,
+  });
+  
+  const mycourse = mongoose.model('mycourse', mycourseSchema);
+  
+  module.exports = mycourse;
